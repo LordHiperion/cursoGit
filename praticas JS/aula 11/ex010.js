e04acd9f44eac02032b27898fc818b4b61e73a61
Fernando <jk157_372@hotmail.com>
2024-08-16 18:19:13 -0300

    progresso nos treinos de JS

---
+++ praticas JS/aula 11/ex010.js
@@ -0,0 +1,6 @@
+var vel = 70.5
+console.log(`a velocidade do carro é ${vel}KM/H`)
+if (vel > 60) { // condição simples
+    console.log('acima do limite de velocidade. MULTADO!')
+}
+console.log('Dirija usando cinto')
