3adea5a5b34ea210faeae7d09535d12dfe818839
Fernando <jk157_372@hotmail.com>
2024-10-21 14:41:27 -0300

    alterascoes

--- praticas JS/aula 11/ex011.js
+++
@@ -1,7 +0,0 @@
-//TODO: fazer esse exercicio em html
-var pais = 'EUA'
-if (pais == 'Brasil') {
-    console.log( 'Brasileiro')
-}else{ //condição composta composta
-    console.log('você é estrangeriro')
-}
